41691350130fe09b336ab0b0e5099f3e9a3bafb8
Sabastian Mugazambi <mugazambi@gmail.com>
2014-10-03 21:59:44 -0500

    Initial commit

---
+++ Hydra.java
@@ -0,0 +1,10 @@
+// By Sabastian Mugazambi 
+//Using 
+
+public class Hydra {
+
+public static void main(String[] args){
+	BagInterface<String> heads = new ArrayBag<String>(200000);
+		heads.add(String "HYDRA")
+		System.out.println(heads)
+}
